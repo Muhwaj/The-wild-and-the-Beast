66c9da9790e819e9c6f995a28cda62352ae69cab
Muhwaj <muhabawajid@gmail.com>
2025-12-10 23:43:56 +0300

    commit 5

--- Game version draft 1 python.py
+++ Game version draft 1 python.py
@@ -23,16 +23,19 @@ class Game:
         
         while True: 
             try: 
-                choice = int(input("Choose wisely:"))
+                choice = int(input("\nChoose wisely:" ))
                 if 1 <= choice <= len(choices):
                     return choice
                 else: 
                     print("The choice is unavailable. Try again")
             except ValueError:
                 print("Enter a number please")
+                
+                
 #opening scene. Screen lights up in a way as in one opening their eyes. 
 #MC slowly opens eyes to see leaves on trees and sunshine pouring through.
 #MC gets up and looks around while narration is played. 
+
     def opening(self):
         self.clear_screen()
         self.display_text("""
@@ -62,8 +65,6 @@ because leading down a path you haven't yet explored, you've found tracks. Fresh
         choices = [
             "Follow the tracks...",
             "Remember the trials...",
-            "Remember when you almost died..." #mught change this option later or take it out i have no idea 
-            
         ]
         
         choices = self.get_choice(choices)
@@ -72,8 +73,7 @@ because leading down a path you haven't yet explored, you've found tracks. Fresh
             self.get_attacked_by_wolves()
         elif choices == 2:
             self.flashback_trials()
-        elif choices == 3:
-            ############ flashback to something else maybe??
+        
         
     def get_attacked_by_wolves(self):
         self.clear_screen()
@@ -84,10 +84,72 @@ You're close. You feel it.
  
 You continue down the path, not daring to breathe. 
 For the first time you thank the moss that conceals the sound of your footsteps. 
+
+All is quiet... until you hear something. It sounds like water. Rushing water. 
+As you look ahead you notice a cliff that drops down. You step closer and look down. 
+Its a massive ravine with a giant rushing river.
+
+The beast probably doesn't live down there-
+
+Suddenly a howl pierces the air. The another one. Then six more. You freeze. 
+
+6 humoungous wolves emerge from the forest. 6 pairs of yellow eyes pin you down and surround you from each side.
+A chill runs down your spine. 
+
+You have no path for escape. 
+Behind you is the ravine. In front of you...the wolves.
+
+This is the end... 
+You close your eyes and wait for the imminent attack 
+
                           
+        """)
+#after the words "imminent attack" the screen should go dark and make it seem like the game ended
+
+
+
+        choices = [
+            "Rush in to help fight"
+            "Stand back. You'll both die if you help"
+        ]
+        
+        choices= self.get_choice(choices)
+        
+        if choices == 1 :
+            self.state ['Helped_Twin_A'] = True
+        else:
+            self.state ['Helped_Twin_A'] = False
+            
+    def fight_together(self):
+        self.clear_screen()
+        self.display_text ("""
+                           
+THE FIGHT WITH THE VWOLVES- HELPING TWIN VERSION 
+                           
+                           
+            """)
+        
+        input("\nPress enter to continue...")
+        self.journey_to_treehouse()
+        
+    def stand_back(self):
+        self.clear_screen()
+        self.display_text("""
+    DONT HELP THE TWIN AND JUST WATCH THEM GET HURT LOLOLOL
+           
                           
                           """)
-#########RAH I DONT WANNA WORK ON THIS FOR NOW I WANNA MAKE THE ARRTTTT okAY nvm lock in 
+        
+        input("\n Press Enter to continue...")
+        self.journey_to_treehouse()
+        
+    def journey_to_treehouse(self):
+        self.clear_screen()
+        self.display_text(""" 
+                          
+    JOURNEY TO TREEHOUSE. INTRODUCTION TO RENN'S PERSONALITY
+                          """)
+        
         
         
         
